6f5d6658faa3fb9a6fc6862c00b15b5de634135f
M.C. van den Boogaart <scola@kvdb.name>
2017-05-11 17:30:30 +0200

    Add option to get height/width from style

--- src/height.js
+++ src/height.js
@@ -1,5 +1,14 @@
 import { selection } from 'd3';
 
-selection.prototype.height = function height() {
-  return this.node().getBoundingClientRect().height;
+selection.prototype.height = function height(style = false) {
+  if (style === true) {
+    const value = this.style('height');
+    return value.match('px') === null ?
+      NaN : parseFloat(value);
+  }
+
+  return this
+    .node()
+    .getBoundingClientRect()
+    .height;
 };
--- src/width.js
+++ src/width.js
@@ -1,5 +1,14 @@
 import { selection } from 'd3';
 
-selection.prototype.width = function width() {
-  return this.node().getBoundingClientRect().width;
+selection.prototype.width = function width(style = false) {
+  if (style === true) {
+    const value = this.style('width');
+    return value.match('px') === null ?
+      NaN : parseFloat(value);
+  }
+
+  return this
+    .node()
+    .getBoundingClientRect()
+    .width;
 };
